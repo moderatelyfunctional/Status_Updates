3ca53c3ce56b9e55519ec18a1f4d17dc1a5cb151
Jing Lin <Jinglin@mit.edu>
2017-12-25 18:38:33 -0500

    git ignoring file

---
+++ Status_Updates/Status_Updates/KEYS.swift
@@ -0,0 +1,9 @@
+//
+//  KEYS.swift
+//  Status_Updates
+//
+//  Created by Jing Lin on 12/25/17.
+//  Copyright © 2017 Jing Lin. All rights reserved.
+//
+
+import Foundation
---
+++ Status_Updates/Status_Updates/WeatherAPI.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPI.swift
+//  Status_Updates
+//
+//  Created by Jing Lin on 12/25/17.
+//  Copyright © 2017 Jing Lin. All rights reserved.
+//
+
+import Foundation
+
+class WeatherAPI {
+    
+    
+    
+    
+    
+}
