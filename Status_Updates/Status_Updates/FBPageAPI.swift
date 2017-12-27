ac796139d5ec028fc1061964a9990e12ebccd5b6
Alisa Ono <alisao@mit.edu>
2017-12-27 16:23:45 -0500

    Initializing the PageList with actual data from API

--- Status_Updates/Status_Updates/FBPageAPI.swift
+++ Status_Updates/Status_Updates/FBPageAPI.swift
@@ -10,10 +10,62 @@ import Foundation
 
 class FBPageAPI {
     
+    struct PageMeta {
+        let name:String
+        let likes:Int
+    }
+
+    struct PagePosts {
+        let updated:NSDate
+        let nPosts:Int
+    }
+
     let BASE_URL = KEYS.FB_BASE_URL
     let ACCESS_TOKEN = KEYS.FB_ACCESS_TOKEN
     
-    func pageFromJSON(_ data: Data) -> Page? {
+    func metaFromJSON(_ data: Data) -> PageMeta? {
+        let json:JSON
+        
+        do {
+            json = try JSONSerialization.jsonObject(with: data, options: []) as! JSON
+        } catch {
+            NSLog("JSON parsing failed: \(error)")
+            return nil
+        }
+        
+        let pageName = json["name"]
+        let pageLikes = json["fan_count"]
+        let pageMeta = PageMeta(name: pageName as! String, likes: pageLikes as! Int)
+        
+        return pageMeta
+    }
+    
+    func fetchMeta(_ query: String, success: @escaping (PageMeta) -> Void) {
+        let session = URLSession.shared
+        let rawURLStr = "\(BASE_URL)/\(query)?access_token=\(ACCESS_TOKEN)&fields=name,fan_count"
+        let escapedURL = rawURLStr.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
+        let url = URL(string: escapedURL!)
+        let task = session.dataTask(with: url!) { data, response, err in
+            if let error = err {
+                NSLog("FB API Error: \(error)")
+            }
+            if let httpResponse = response as? HTTPURLResponse {
+                switch httpResponse.statusCode {
+                case 200:
+                    if let pageMeta = self.metaFromJSON(data!) {
+                        success(pageMeta)
+                    }
+                case 401:
+                    NSLog("FB API Unauthorized Error")
+                default:
+                    NSLog("FB API returned response: %d %@", httpResponse.statusCode, HTTPURLResponse.localizedString(forStatusCode: httpResponse.statusCode))
+                }
+            }
+        }
+        task.resume()
+    }
+
+    func postsFromJSON(_ data: Data) -> PagePosts? {
         let json:JSON
         
         do {
@@ -24,17 +76,14 @@ class FBPageAPI {
         }
         
         let postList = json["data"] as! [JSON]
-        let postDict = postList[0]
+        // let postUpdated = postList[0]["created_time"] as! NSDate
+        let postUpdated = NSDate(timeIntervalSinceNow: TimeInterval(exactly: 11000.0)!)
+        let pagePosts = PagePosts(updated: postUpdated, nPosts: postList.count)
         
-        let page = Page(time: postDict["created_time"] as! String,
-                        content: postDict["message"] as! String,
-                        ID: postDict["id"] as! String)
-        
-        return page
+        return pagePosts
     }
-    
 
-    func fetchPage(_ query: String, success: @escaping (Page) -> Void) {
+    func fetchPosts(_ query: String, success: @escaping (PagePosts) -> Void) {
         let timeUntil = round(NSDate().timeIntervalSince1970)
         let day:TimeInterval = 60 * 60 * 24
         let timeSince = timeUntil - day
@@ -49,8 +98,8 @@ class FBPageAPI {
             if let httpResponse = response as? HTTPURLResponse {
                 switch httpResponse.statusCode {
                 case 200:
-                    if let page = self.pageFromJSON(data!) {
-                        success(page)
+                    if let pagePosts = self.postsFromJSON(data!) {
+                        success(pagePosts)
                     }
                 case 401:
                     NSLog("FB API Unauthorized Error")
@@ -60,5 +109,14 @@ class FBPageAPI {
             }
         }
         task.resume()
+    }
+    
+    func fetchPage(_ query: String, success: @escaping (PageData) -> Void) {
+        self.fetchMeta(query) { pageMeta in
+            self.fetchPosts(query) { pagePosts in
+                let pageData = PageData(name: pageMeta.name, likes: pageMeta.likes, updated: pagePosts.updated, nPosts: pagePosts.nPosts)
+                success(pageData)
+            }
+        }
     }
 }
--- Status_Updates/Status_Updates/PageList.swift
+++ Status_Updates/Status_Updates/PageList.swift
@@ -11,23 +11,28 @@ import Foundation
 final class PageList {
     
     static let sharedInstance = PageList()
-    
-    let data = [PageData(name: "MIT Confessions",
-                         likes: 18940,
-                         updated: NSDate(timeIntervalSinceNow: TimeInterval(exactly: 2400.0)!),
-                         nPosts: 4),
-                PageData(name: "Overheard At MIT",
-                         likes: 2242,
-                         updated: NSDate(timeIntervalSinceNow: TimeInterval(exactly: 11000.0)!),
-                         nPosts: 10),
-                PageData(name: "MIT Onion",
-                         likes: 18940,
-                         updated: NSDate(timeIntervalSinceNow: TimeInterval(exactly: 3720.0)!),
-                         nPosts: 0)
-    ]
+    var data = [PageData]()
     
     private init() {
-        
+        self.data = fetchData(pages: ["beaverconfessions", "overheardatmit", "TheMITOnion"])
     }
-    
+
+    private func fetchData(pages: [String]) -> [PageData] {
+        var pageDataList = [PageData]()
+        let fbPageAPI = FBPageAPI()
+        let group = DispatchGroup()
+        
+        for page in pages {
+            group.enter()
+            DispatchQueue.global().async {
+                fbPageAPI.fetchPage(page) { pageData in
+                    pageDataList.append(pageData)
+                    group.leave()
+                }
+            }
+            group.wait()
+        }
+        
+        return pageDataList
+    }
 }
